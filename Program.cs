5a39dbe11b5335a4f90983164f610afbbe1c39d0
Alexander <mypost@example.com>
2023-01-21 19:38:57 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41. Вариант 1 через массив и функцию
+int FuncCheck(int[] arr)
+{
+    int val_2 = 0;
+    for (int o = 0; o < arr.Length; o++){
+        if (arr[o] > 0) val_2 = val_2 + 1;
+    }
+    return val_2;
+}
+
+Console.WriteLine($"Укажите, сколько чисел будет в вашем массиве");
+int val_1 = Convert.ToInt32(Console.ReadLine());
+int [] Array = new int[val_1];
+Console.WriteLine($"Вводите числа. Заполнив массив, программа продолжит работу.");
+
+for (int i = 0; i < val_1; i++)
+{
+    int temp = Convert.ToInt32(Console.ReadLine());
+    Array[i] = temp;
+}
+
+Console.WriteLine($"Количество чисел больше 0 - {FuncCheck(Array)}");
